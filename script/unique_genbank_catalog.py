e05b69d232d8bc53b299d8b13c5f397350992498
Danilo Horta <danilo.horta@pm.me>
2020-07-20 13:30:04 +0100

    dask

--- script/unique_genbank_catalog.py
+++ script/unique_genbank_catalog.py
@@ -1,7 +1,7 @@
 import sys
 from collections import OrderedDict
 
-import pandas as pd
+import dask.dataframe as dd
 
 dtype = OrderedDict()
 dtype["Accession"] = "str"
@@ -19,16 +19,23 @@ input_file = sys.argv[1]
 output_file = sys.argv[2]
 
 names = list(dtype.keys())
-df = pd.read_csv(input_file, sep="\t", header=0, dtype=dtype)
+df = dd.read_csv(input_file, sep="\t", header=0, dtype=dtype)
+# df = pd.read_csv(input_file, sep="\t", header=0, dtype=dtype)
 # df = pd.read_csv(input_file, sep="\t", header=None, names=names, dtype=dtype)
 
 max_bp = {key: row.BasePairs for key, row in df.groupby("Organism").max().iterrows()}
-df["longest"] = df.apply(lambda row: max_bp[row.Organism] == row.BasePairs, axis=1)
-df = df[df["longest"]].copy()
+df["longest"] = df.apply(
+    lambda row: max_bp[row.Organism] == row.BasePairs, axis=1, meta=bool
+)
+# df["longest"] = df.apply(lambda row: max_bp[row.Organism] == row.BasePairs, axis=1)
+df = df[df["longest"]]
 
-max_id = {key: row.ID for key, row in df.groupby("Organism").max().iterrows()}
-df["latest"] = df.apply(lambda row: max_id[row.Organism] == row.ID, axis=1)
-df = df[df["latest"]].copy()
+# max_id = {key: row.ID for key, row in df.groupby("Organism").max().iterrows()}
+# df["latest"] = df.apply(lambda row: max_id[row.Organism] == row.ID, axis=1)
+# df = df[df["latest"]].copy()
+df = df.drop_duplicates(subset=["Organism"], keep="last")
+df = df.drop(columns=["longest"])
 
-df = df.reset_index(drop=True)
-df.to_feather(output_file)
+# df = df.reset_index(drop=True)
+# df.to_feather(output_file)
+df.to_csv(output_file, encoding="ascii", sep="\t", single_file=True, index=False)
